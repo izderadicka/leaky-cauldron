3365e2ae6fd84f579a7c229acedf45dc8ba0e83c
Ivan <ivan.zderadicka@gmail.com>
2021-02-06 10:29:32 +0100

    ticker

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,84 @@
 use std::{
+    future::Future,
+    pin::Pin,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
     },
+    task::{Context, Poll},
     time::Duration,
 };
 
-use futures::future::{select, Either};
-use log::{debug, error, trace, warn};
-use tokio::{sync::mpsc::{unbounded_channel, UnboundedSender}, time::{Instant, Sleep, sleep_until}};
+use futures::{
+    future::poll_fn
+};
+use log::{error, trace, warn};
+use tokio::{
+    sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+    time::{Instant, Sleep},
+};
 
 #[derive(Debug, Clone, Copy)]
 enum Cmd {
     Terminate,
+    Pause,
     Continue,
+}
+
+struct Ticker {
+    timer: Option<Pin<Box<Sleep>>>,
+    cmd: Pin<Box<UnboundedReceiver<Cmd>>>,
+    period: Duration,
+}
+
+impl Ticker {
+    fn new(period: Duration, cmd_channel: UnboundedReceiver<Cmd>) -> Self {
+        Ticker {
+            timer: Some(Box::pin(tokio::time::sleep(period))),
+            cmd: Box::pin(cmd_channel),
+            period,
+        }
+    }
+    fn poll_tick(&mut self, ctx: &mut Context) -> Poll<Option<Instant>> {
+        match Pin::new(&mut self.cmd).poll_recv(ctx) {
+            Poll::Ready(Some(cmd)) => match cmd {
+                Cmd::Terminate => return Poll::Ready(None),
+                Cmd::Pause => {
+                    self.timer.take();
+                }
+                Cmd::Continue => self.timer = Some(Box::pin(tokio::time::sleep(self.period))),
+            },
+            Poll::Ready(None) => {
+                warn!("Channel should not be closed, rather use terminate message");
+                return Poll::Ready(None);
+            }
+            Poll::Pending => {}
+        }
+        match self.timer.take() {
+            Some(mut timer) => {
+                if let Poll::Pending = Pin::new(&mut timer).poll(ctx) {
+                    self.timer = Some(timer);
+                    return Poll::Pending;
+                }
+                let t = timer.deadline();
+                let next_tick = t + *&self.period;
+                timer.as_mut().reset(next_tick);
+                self.timer = Some(timer);
+                Poll::Ready(Some(t))
+            }
+            None => Poll::Pending,
+        }
+    }
+
+    async fn tick(&mut self) -> Option<Instant> {
+        poll_fn(|cx| self.poll_tick(cx)).await
+    }
 }
 
 pub struct Leaky {
     counter: Arc<AtomicUsize>,
     capacity: usize,
     sender: UnboundedSender<Cmd>,
-}
-
-const LONG_DURATION: Duration = Duration::from_secs(3*365*24*3600);
-
-#[inline]
-fn sleep(period: Duration) -> Option<(Instant, Sleep)> {
-    let t =  Instant::now();
-    Some((t, sleep_until(t+period)))
-
 }
 
 impl Leaky {
@@ -38,51 +88,29 @@ impl Leaky {
         assert!(capacity > 0);
         let interval_ms: u64 = (KILO / rate) as u64;
         let counter = Arc::new(AtomicUsize::new(0));
-        let (sender, mut recipient) = unbounded_channel();
+        let (sender, recipient) = unbounded_channel();
         let counter2 = counter.clone();
+        let sender2 = sender.clone();
         let _t = tokio::spawn(async move {
             let period = Duration::from_millis(interval_ms);
-            debug!("period {:?}", period);
-            let mut next_time = sleep(period);
-            loop {
-                let (prev_time, next_tick) = next_time.take().unwrap();
-                let tick = Box::pin(next_tick);
-                let recv = Box::pin(recipient.recv());
-                match select(tick, recv).await {
-                    Either::Left((_, _r)) => {
-                        let endured =Instant::now().duration_since(prev_time).as_millis();
-                        let mut ratio = (endured as u64 / interval_ms) as usize;
-                        if ratio > 1 {
-                            warn!("has been waiting too long {}x", ratio)
-                        } else if ratio == 0 {
-                            warn!("has been waiting too shoot {}", endured);
-                            // we always want to proceed
-                            ratio =1
-                        }
-                        let v = counter2.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| {
-                            if v > 0 {
-                                Some(v.saturating_sub(ratio))
-                            } else {
-                                None
-                            }
-                        });
-                        trace!("Tick for old v {:?}", v);
-                        match v {
-                            Ok(1) | Err(0) => next_time = { 
-                                sleep(LONG_DURATION)
-                            },
-                            _ => next_time = {
-                                sleep(period)
-                            }
-                        }
+            let mut ticker = Ticker::new(period, recipient);
+            while let Some(_t) = ticker.tick().await {
+
+                let v = counter2.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| {
+                    if v > 0 {
+                        Some(v.saturating_sub(1))
+                    } else {
+                        None
                     }
-                    Either::Right((cmd, _)) => match cmd {
-                        Some(Cmd::Continue) => next_time = {
-                            sleep(period)
-                        },
-                        Some(Cmd::Terminate) | None => break,
-                    },
-                };
+                });
+                trace!("Tick for old v {:?}", v);
+                match v {
+                    Ok(1) | Err(0) => {
+                        trace!("Pausing leaking because empty");
+                        sender2.send(Cmd::Pause).expect("ticker is not running");
+                    }
+                    _ => {}
+                }
             }
         });
         Leaky {
@@ -139,9 +167,12 @@ mod tests {
     };
 
     use log::{debug, trace};
-    use tokio::time::{interval, sleep};
+    use tokio::{
+        sync::mpsc::unbounded_channel,
+        time::{interval, sleep},
+    };
 
-    use crate::Leaky;
+    use crate::{Cmd, Leaky, Ticker};
 
     type Error = Box<dyn std::error::Error + Send + 'static>;
     #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
@@ -184,10 +215,10 @@ mod tests {
             }
         }
         // wait a bit for leak:
-        sleep(Duration::from_millis(40)).await;
+        sleep(Duration::from_millis(30)).await;
         let res = leaky.start_one();
         if let Ok(n) = res {
-            assert!(n>48 && n <= 50, "should release one or two slots");
+            assert_eq!(n, 50, "should release one slot");
         } else {
             panic!("Slot was not released by leaky")
         }
@@ -198,34 +229,73 @@ mod tests {
         let res = leaky.start_one();
         if let Ok(n) = res {
             debug!("Taken slots after 300ms {}", n);
-            assert!(n < 50 - 10, "taken slots should decrease by at least 10");
+            assert!(n <= 50 - 14, "taken slots should decrease by at least 14");
         } else {
             panic!("Slot was not released by leaky")
         }
     }
 
-    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    #[tokio::test]
     async fn test_leaky_pausing() {
         env_logger::try_init().ok();
         let leaky = Leaky::new(10, 500.0);
-        for _i in 1..=10 {
-            assert!(leaky.start_one().is_ok());
-        }
-        //should be full now
-        assert!(leaky.start_one().is_err());
-        sleep(Duration::from_millis(50)).await;
-        assert_eq!(leaky.immediate_capacity(), 10);
-        sleep(Duration::from_millis(200)).await;
 
+        macro_rules! tst {
+            () => {
+
+                for _i in 1..=10 {
+                    assert!(leaky.start_one().is_ok());
+                }
+                //should be full now
+                assert!(leaky.start_one().is_err());
+                sleep(Duration::from_millis(70)).await;
+                assert_eq!(leaky.immediate_capacity(), 10);
+
+            }
+        }
+        
+        tst!();
+
+        sleep(Duration::from_millis(200)).await;
         // again
 
-        for _i in 1..=10 {
-            assert!(leaky.start_one().is_ok());
-        }
-        //should be full now
-        assert!(leaky.start_one().is_err());
-        sleep(Duration::from_millis(50)).await;
-        assert_eq!(leaky.immediate_capacity(), 10);
+        tst!();
+    }
 
+    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    async fn test_ticker() {
+        env_logger::try_init().ok();
+        let (sender, receiver) = unbounded_channel();
+        const PERIOD: u64 = 20;
+        let mut ticker = Ticker::new(Duration::from_millis(PERIOD), receiver);
+        let start = Instant::now();
+        let mut count = 1;
+        while let Some(t) = ticker.tick().await {
+            let d = t.duration_since(start.into());
+            let diff = d.as_secs_f64() * 1000.0 - (count * PERIOD) as f64;
+            trace!("tick {} time {:?} diff {}", count, d, diff);
+            if count <= 10 {
+                assert!(
+                    diff.abs() < 0.1,
+                    "difference from theoretical start is less then 0.1 ms"
+                );
+            }
+
+            if count == 10 {
+                sender.send(Cmd::Pause).ok();
+                let s2 = sender.clone();
+                tokio::spawn(async move {
+                    tokio::time::sleep(Duration::from_millis(100)).await;
+                    s2.send(Cmd::Continue)
+                });
+            }
+
+            if count == 20 {
+                sender.send(Cmd::Terminate).ok();
+            } else {
+                count += 1;
+            }
+        }
+        assert_eq!(count, 20);
     }
 }
